f13660b1fbe4c0d0161853d24dcd964357b52cca
Tiagoa <tiagoa@tiagoa-laptop.(none)>
2011-06-02 21:27:07 -0300

    Adicao do alerta sobre a conexão com a internet.

--- Resources/app.js
+++ Resources/app.js
@@ -1,4 +1,8 @@
 var webview = Titanium.UI.createWebView({url:'index.htm'});
 var window = Titanium.UI.createWindow();
+var statusConexao = Titanium.Network.networkTypeName;
 window.add(webview);
 window.open();
+if(statusConexao == 'NONE'){
+	alert('Você precisa estar conectado à internet para consultar o horário.');
+}
